c4da0f4aa34265615c83f41d07e56c1398712303
Alan Wu <mlwu860401@gmail.com>
2020-07-12 15:58:41 -0400

    Updates

---
+++ precache-manifest.e7338abd0fdbf8c4169d9acca4d7e1d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae36193ef815c1480a45e4d20b7d1b8",
+    "url": "/react_sample/index.html"
+  },
+  {
+    "revision": "4b7a5ae02f8530d2dab4",
+    "url": "/react_sample/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "90ee0e28e22473810112",
+    "url": "/react_sample/static/css/main.41c75463.chunk.css"
+  },
+  {
+    "revision": "4b7a5ae02f8530d2dab4",
+    "url": "/react_sample/static/js/2.028b13a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_sample/static/js/2.028b13a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ee0e28e22473810112",
+    "url": "/react_sample/static/js/main.b9fa23b4.chunk.js"
+  },
+  {
+    "revision": "697a41f49510b37c3c0a",
+    "url": "/react_sample/static/js/runtime-main.4e9da1dc.js"
+  },
+  {
+    "revision": "9d6eb4ad3c0ea27f4db631a4c9698c05",
+    "url": "/react_sample/static/media/SHBELSX-TY-2.9d6eb4ad.jpg"
+  },
+  {
+    "revision": "5b696cacfa1a33a81d3357099e2ade22",
+    "url": "/react_sample/static/media/SHBELSX-TY-Sole.5b696cac.jpg"
+  },
+  {
+    "revision": "ab2353cc46966b4f93db765bd18aa985",
+    "url": "/react_sample/static/media/SHBELSX-TY.ab2353cc.jpg"
+  },
+  {
+    "revision": "79ad2380ce3f9cdbb8c66eef80bfeaba",
+    "url": "/react_sample/static/media/badminton-wallpaper.79ad2380.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_sample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef147b8543e92d4663c0992e4e5f3dfe",
+    "url": "/react_sample/static/media/logo.ef147b85.jpg"
+  },
+  {
+    "revision": "c4b847c076b9d19b968438d06564d4d6",
+    "url": "/react_sample/static/media/ronney.c4b847c0.jpg"
+  }
+]);
